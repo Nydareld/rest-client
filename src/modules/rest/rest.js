449ca4ff2bb8628c610cf39915d1fa14d75e90be
Théo Guerin <theo.guerin.pro@gmail.com>
2018-02-06 09:35:14 +0100

    panel rest

--- src/modules/rest/rest.js
+++ src/modules/rest/rest.js
@@ -1,3 +1,21 @@
 angular.module("rest-client")
-.controller("restController",[function(){
+.controller("restController",[ "$scope","$http", function($scope,$http){
+
+    var requestHandler = function(res) {
+        $scope.data = res.data;
+        $scope.headers = res.headers;
+        $scope.raw = res;
+
+    }
+
+    $scope.sendRequst = function(){
+        $http({
+            method : $scope.config.method || "GET",
+            url : $scope.config.url || "localhost:80",
+            data : $scope.config.url || null
+        }).then(requestHandler)
+        .catch(requestHandler);
+
+    };
+
 }]);
