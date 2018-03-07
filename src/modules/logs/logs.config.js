7cbef006f236e288f5b0f7484db072a4269cf9e0
Théo Guerin <theo.guerin.pro@gmail.com>
2018-03-07 16:03:21 +0100

    debut logs

---
+++ src/modules/logs/logs.config.js
@@ -0,0 +1,20 @@
+angular.module("rest-client")
+.config(["$stateProvider", "menuProvider" , function($stateProvider  ,  menuProvider){
+    console.log("here");
+    // enregistrement de l'etat logs
+    $stateProvider.state("rest-client.logs",{
+        entryName : "Logs",
+        url: "/logs",
+        templateUrl: "./modules/logs/logs.html",
+        controller : "logsController"
+    });
+
+    // enregistrement de l'entrée mennue Logs
+    menuProvider.add({
+        stateName : "rest-client.logs",
+        name : "Logs",
+        icon : "terminal"
+    });
+
+
+}]);
---
+++ src/modules/logs/logs.js
@@ -0,0 +1,22 @@
+angular.module("rest-client")
+.controller("logsController",[ "$scope","$http", function($scope,$http){
+    $scope.config={};
+
+    var requestHandler = function(res) {
+        $scope.data = res.data;
+        $scope.headers = res.headers;
+        $scope.raw = res;
+
+    }
+
+    $scope.sendRequst = function(){
+        $http({
+            method : $scope.config.method || "GET",
+            url : $scope.config.url || "localhost:80",
+            data : $scope.config.data || null
+        }).then(requestHandler)
+        .catch(requestHandler);
+
+    };
+
+}]);
