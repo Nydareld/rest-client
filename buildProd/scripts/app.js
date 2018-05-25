98c8f0d0a14d25c11713d161665d6872b72de52e
Théo Guerin <theo.guerin.pro@gmail.com>
2018-05-25 15:03:25 +0200

    auto build :dog:

---
+++ buildProd/scripts/app.js
@@ -0,0 +1,247 @@
+/**
+ * Déclaration du module
+ */
+angular.module("rest-client", [
+    "ui.router", // Routing
+    "ui.bootstrap", // Ui Bootstrap
+    "ngSanitize", // Sanitize
+    "pascalprecht.translate", // Translate
+    "nrcomInspinia", // Theme custom insmina
+    "ui.ace"
+]);
+
+var modulesToRun = ["rest-client"];
+
+/**
+ * Configuration de base:
+ *      - Route par defaut
+ *      - Mode html5 des url
+ *      - Etat root
+ */
+angular
+    .module("rest-client")
+    .config([
+        "$stateProvider",
+        "$urlRouterProvider",
+        "$locationProvider",
+        function($stateProvider, $urlRouterProvider, $locationProvider) {
+            // Configuration du mode HTML5 de locationProvider
+            $locationProvider
+                .html5Mode({
+                    enabled: false,
+                    requireBase: true,
+                    rewriteLinks: true
+                })
+                .hashPrefix("");
+
+            // url par defaut : /404
+            $urlRouterProvider.otherwise("/404");
+
+            // Déclaration de l'index, noeud de base duquel heriteront d'autre noeuds
+            $stateProvider.state("rest-client", {
+                abstract: true,
+                controller: "RootController",
+                templateUrl: "./modules/common/content.html"
+            });
+        }
+    ])
+    .run(function($rootScope, $state) {
+        // Lancement de la machien a etat
+        $rootScope.$state = $state;
+    });
+
+angular.module("rest-client").constant("globals", {
+    api: {
+        baseUrl: "http://localhost/api/v1"
+    },
+    debug: true,
+    debugRouter: false,
+    sampleData: true
+});
+
+angular.module("rest-client").config([
+    "$stateProvider",
+    function($stateProvider) {
+        $stateProvider.state("rest-client.404", {
+            url: "/404",
+            templateUrl: "./modules/404/404.html"
+        });
+    }
+]);
+
+angular.module("rest-client").controller("RootController", [
+    "$rootScope",
+    function($rootScope) {
+        this.logo = {
+            miniText: "test",
+            img: "statics/img/icon.png"
+        };
+        this.user = {
+            username: "Theo Guerin",
+            logout: "/logout"
+        };
+        this.menu = $rootScope.menu;
+        this.helloText = "Test layout Inspinia";
+        this.descriptionText =
+            "Ceci est un exemple/test destiné a tester le layout ";
+    }
+]);
+
+angular.module("rest-client").config([
+    "$stateProvider",
+    "$urlRouterProvider",
+    "menuProvider",
+    function($stateProvider, $urlRouterProvider, menuProvider) {
+        $stateProvider.state("rest-client.index", {
+            entryName: "Accueil",
+            url: "/index",
+            controller: "indexController",
+            templateUrl: "./modules/index/index.html"
+        });
+
+        menuProvider.add({
+            stateName: "rest-client.index",
+            icon: "home",
+            name: "Accueil"
+        });
+    }
+]);
+
+angular.module("rest-client").controller("indexController", [function() {}]);
+
+angular.module("rest-client").config([
+    "$stateProvider",
+    function($stateProvider) {
+        $stateProvider.state("login", {
+            url: "/login",
+            templateUrl: "./modules/login/login.html"
+        });
+    }
+]);
+
+angular.module("rest-client").config([
+    "$stateProvider",
+    "menuProvider",
+    function($stateProvider, menuProvider) {
+        console.log("here");
+        // enregistrement de l'etat logs
+        $stateProvider.state("rest-client.logs", {
+            entryName: "Logs",
+            url: "/logs",
+            templateUrl: "./modules/logs/logs.html",
+            controller: "logsController"
+        });
+
+        // enregistrement de l'entrée mennue Logs
+        menuProvider.add({
+            stateName: "rest-client.logs",
+            name: "Logs",
+            icon: "terminal"
+        });
+    }
+]);
+
+angular.module("rest-client").controller("logsController", [
+    "$scope",
+    "$http",
+    function($scope, $http) {
+        $scope.config = {};
+
+        var requestHandler = function(res) {
+            $scope.data = res.data;
+            $scope.headers = res.headers;
+            $scope.raw = res;
+        };
+
+        $scope.formName = "customerForm";
+        $scope.params = {
+            limit: 25,
+            start: 0
+        };
+        $scope.formStructure = [
+            {
+                title: "Parametres généraux",
+                type: "separator"
+            },
+            {
+                allowBlank: true,
+                label: "Limite",
+                name: "limit",
+                type: "number"
+            },
+            {
+                allowBlank: true,
+                label: "Début",
+                name: "start",
+                type: "number"
+            },
+            {
+                title: "Parametres D'api",
+                type: "separator"
+            },
+            {
+                allowBlank: true,
+                label: "Début",
+                name: "app",
+                type: "choice",
+                items: ["info", "error", "debug", "warning"]
+            }
+        ];
+
+        $scope.getLogs = function(params) {
+            console.log(params);
+            $http({
+                method: "GET",
+                params: params,
+                url: "http://redway.nrco.fr:1880/qual/nrcom/logs"
+            })
+                .then(requestHandler)
+                .catch(requestHandler);
+        };
+    }
+]);
+
+angular.module("rest-client").config([
+    "$stateProvider",
+    "menuProvider",
+    function($stateProvider, menuProvider) {
+        // enregistrement de l'etat rest
+        $stateProvider.state("rest-client.rest", {
+            entryName: "Rest",
+            url: "/rest",
+            templateUrl: "./modules/rest/rest.html",
+            controller: "restController"
+        });
+
+        // enregistrement de l'entrée mennue Rest
+        menuProvider.add({
+            stateName: "rest-client.rest",
+            name: "Rest",
+            icon: "code"
+        });
+    }
+]);
+
+angular.module("rest-client").controller("restController", [
+    "$scope",
+    "$http",
+    function($scope, $http) {
+        $scope.config = {};
+
+        var requestHandler = function(res) {
+            $scope.data = res.data;
+            $scope.headers = res.headers;
+            $scope.raw = res;
+        };
+
+        $scope.sendRequst = function() {
+            $http({
+                method: $scope.config.method || "GET",
+                url: $scope.config.url || "localhost:80",
+                data: $scope.config.data || null
+            })
+                .then(requestHandler)
+                .catch(requestHandler);
+        };
+    }
+]);
---
+++ buildProd/scripts/app.templates.js
@@ -0,0 +1,197 @@
+angular.module('rest-client').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('./modules/404/404.html',
+    "<div class=\"wrapper wrapper-content\">\n" +
+    "    <div class=\"row\">\n" +
+    "        <div class=\"col-lg-12\">\n" +
+    "            <div class=\"text-center m-t-lg\">\n" +
+    "                <h1>\n" +
+    "                    404 Page introuvable\n" +
+    "                </h1>\n" +
+    "                <small>\n" +
+    "                    La page demandée est introuvable, si le problème persiste veuillez contacter le support\n" +
+    "                </small>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>\n"
+  );
+
+
+  $templateCache.put('./modules/common/content.html',
+    "<!-- Wrapper-->\n" +
+    "<div id=\"wrapper\">\n" +
+    "\n" +
+    "    <!-- Navigation -->\n" +
+    "        <left-side-menu logo=\"rootCtrl.logo\" user=\"rootCtrl.user\"></left-side-menu>\n" +
+    "\n" +
+    "    <!-- Page wraper -->\n" +
+    "    <!-- ng-class with current state name give you the ability to extended customization your view -->\n" +
+    "    <div id=\"page-wrapper\" class=\"gray-bg {{$state.current.name}}\">\n" +
+    "\n" +
+    "        <!-- Page wrapper -->\n" +
+    "        <!-- <div ng-include=\"'modules/common/topnavbar.html'\"></div> -->\n" +
+    "        <top-nav-bar search-place-holder=\"Rechercher\"></top-nav-bar>\n" +
+    "\n" +
+    "        <breadcrumbs\n" +
+    "                    home-state-name=\"rest-client.index\"\n" +
+    "                    home-state-text=\"rest client\">\n" +
+    "        </breadcrumbs>\n" +
+    "\n" +
+    "        <!-- Main view  -->\n" +
+    "        <div ui-view></div>\n" +
+    "\n" +
+    "        <!-- Footer -->\n" +
+    "        <footer\n" +
+    "            footer-left=\"<strong>Copyright</strong> Example Company &copy; 2015-2017\"\n" +
+    "            footer-right=\"10GB of <strong>250GB</strong> Free.\"\n" +
+    "\n" +
+    "            ></footer>\n" +
+    "        <!-- <div ng-include=\"'modules/common/footer.html'\"></div> -->\n" +
+    "\n" +
+    "    </div>\n" +
+    "    <!-- End page wrapper-->\n" +
+    "\n" +
+    "    <!-- Right Sidebar -->\n" +
+    "    <!-- <div ng-include=\"'modules/common/right_sidebar.html'\"></div> -->\n" +
+    "\n" +
+    "</div>\n" +
+    "<!-- End wrapper-->\n"
+  );
+
+
+  $templateCache.put('./modules/index/index.html',
+    "<div class=\"wrapper wrapper-content animated fadeInRight\">\n" +
+    "    <div class=\"row\">\n" +
+    "        <div class=\"col-lg-12\">\n" +
+    "            <div class=\"text-center m-t-lg\">\n" +
+    "                <h1>\n" +
+    "                    {{rootCtrl.helloText}}\n" +
+    "                </h1>\n" +
+    "                <small>\n" +
+    "                    {{rootCtrl.descriptionText}}\n" +
+    "                </small>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>\n"
+  );
+
+
+  $templateCache.put('./modules/login/login.html',
+    "<div class=\"middle-box text-center loginscreen animated fadeInDown\">\n" +
+    "    <div>\n" +
+    "        <div>\n" +
+    "\n" +
+    "            <h1 class=\"logo-name\">N-I</h1>\n" +
+    "\n" +
+    "        </div>\n" +
+    "        <h3>Bienvenue dans Nrcom-inspinia</h3>\n" +
+    "        <p>Connectez vous</p>\n" +
+    "        <form class=\"m-t\" role=\"form\" action=\"index.html\">\n" +
+    "            <div class=\"form-group\">\n" +
+    "                <input type=\"email\" class=\"form-control\" placeholder=\"Username\" required=\"\">\n" +
+    "            </div>\n" +
+    "            <div class=\"form-group\">\n" +
+    "                <input type=\"password\" class=\"form-control\" placeholder=\"Password\" required=\"\">\n" +
+    "            </div>\n" +
+    "            <button type=\"submit\" class=\"btn btn-primary block full-width m-b\">Login</button>\n" +
+    "\n" +
+    "            <a href=\"#\"><small>Mot de passe oublié ?</small></a>\n" +
+    "        </form>\n" +
+    "        <p class=\"m-t\"> <small>Inspinia we app framework base on Bootstrap 3 © 2014</small> </p>\n" +
+    "    </div>\n" +
+    "</div>\n"
+  );
+
+
+  $templateCache.put('./modules/logs/logs.html',
+    "<div class=\"wrapper wrapper-content\">\n" +
+    "    <div class=\"ibox\">\n" +
+    "        <div class=\"ibox-title\">\n" +
+    "            <h5>Logs</h5>\n" +
+    "        </div>\n" +
+    "        <div class=\"ibox-content\">\n" +
+    "            <div class=\"\">\n" +
+    "                <form-builder structure=\"formStructure\" submit-callback=\"getLogs\" model=\"params\"></form-builder>\n" +
+    "            </div>\n" +
+    "            <div class=\"form-group\">\n" +
+    "                <label>Resulat</label>\n" +
+    "                <br>\n" +
+    "                <div class=\"input-group\" style=\"min-height : 300px; width:100%; max-width:100%;\">\n" +
+    "                    <pre>{{ data | json }}</pre>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>\n"
+  );
+
+
+  $templateCache.put('./modules/rest/rest.html',
+    "<div class=\"wrapper wrapper-content\">\n" +
+    "    <div class=\"ibox\">\n" +
+    "        <div class=\"ibox-title\">\n" +
+    "            <h5>Client rest</h5>\n" +
+    "        </div>\n" +
+    "        <div class=\"ibox-content\">\n" +
+    "            <div class=\"form-group\">\n" +
+    "                <label>Parametres de route</label>\n" +
+    "                <div class=\"input-group\">\n" +
+    "                    <div class=\"input-group-btn\" ng-init=\"config.method  ='GET'\">\n" +
+    "                        <button type=\"button\" class=\"btn\">{{ config.method }}</button>\n" +
+    "                        <button type=\"button\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
+    "                            <span class=\"caret\"></span>\n" +
+    "                            <span class=\"sr-only\">Toggle Dropdown</span>\n" +
+    "                        </button>\n" +
+    "                        <ul class=\"dropdown-menu\">\n" +
+    "                            <li ng-click=\"config.method='GET'\"><a>GET</a></li>\n" +
+    "                            <li ng-click=\"config.method='POST'\"><a>POST</a></li>\n" +
+    "                            <li ng-click=\"config.method='PUT'\"><a>PUT</a></li>\n" +
+    "                            <li ng-click=\"config.method='DELETE'\"><a>DELETE</a></li>\n" +
+    "                        </ul>\n" +
+    "                    </div>\n" +
+    "                    <input type=\"text\" class=\"form-control\" ng-model=\"config.url\" placeholder=\"URL\" aria-describedby=\"basic-addon1\">\n" +
+    "                    <span class=\"input-group-btn\">\n" +
+    "                        <button class=\"btn btn-default\" ng-click=\"sendRequst()\" type=\"button\">Envoyer</button>\n" +
+    "                    </span>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"form-group\">\n" +
+    "                <label>Contenu</label>\n" +
+    "                <div class=\"input-group\" style=\"height : 300; width:100%;\">\n" +
+    "                    <div ng-model=\"config.data\" style=\"height : 300px; width:100%;\" ui-ace=\"{}\"></div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"form-group\">\n" +
+    "                <label>Resulat</label>\n" +
+    "                <br>\n" +
+    "                <h2 class=\"inspinia-text-primary\">{{ raw.status }} <small> {{ raw.statusText }}</small> </h2>\n" +
+    "                <uib-tabset active=\"0\">\n" +
+    "                    <uib-tab index=\"0\" heading=\"Body\">\n" +
+    "                        <div class=\"input-group\" style=\"min-height : 300px; width:100%; max-width:100%;\">\n" +
+    "                            <pre>{{ data | json }}</pre>\n" +
+    "                        </div>\n" +
+    "                    </uib-tab>\n" +
+    "                    <uib-tab index=\"1\" heading=\"Config\">\n" +
+    "                        <div class=\"input-group\" style=\"min-height : 300px; width:100%; max-width:100%;\">\n" +
+    "                            <pre>{{ config | json }}</pre>\n" +
+    "                        </div>\n" +
+    "                    </uib-tab>\n" +
+    "                    <uib-tab index=\"2\" heading=\"Raw\">\n" +
+    "                        <div class=\"input-group\" style=\"min-height : 300px; width:100%; max-width:100%;\">\n" +
+    "                            <pre>{{ raw | json }}</pre>\n" +
+    "                        </div>\n" +
+    "                    </uib-tab>\n" +
+    "                </uib-tabset>\n" +
+    "\n" +
+    "            </div>\n" +
+    "\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>\n"
+  );
+
+}]);
