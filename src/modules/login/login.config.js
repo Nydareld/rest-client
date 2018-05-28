b4f5d66ad1bc999da8f1452851d2517e4f70fca5
Sebastian Guesdon <sebastian.guesdon@nr-communication.fr>
2018-05-28 16:42:47 +0200

    supression module login

--- src/modules/login/login.config.js
+++
@@ -1,9 +0,0 @@
-angular.module("rest-client").config([
-    "$stateProvider",
-    function($stateProvider) {
-        $stateProvider.state("login", {
-            url: "/login",
-            templateUrl: "./modules/login/login.html"
-        });
-    }
-]);
