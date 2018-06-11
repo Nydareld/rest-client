51d452e0a380c0112382f77279e5ee0b6d9e7dc0
Sebastian Guesdon <sebastian.guesdon@nr-communication.fr>
2018-06-11 17:07:49 +0200

    ajout du module chargé de la gestion des types d'erreurs

---
+++ src/modules/errorTypes/errorTypes.config.js
@@ -0,0 +1,30 @@
+angular.module("rest-client").config([
+    "$stateProvider",
+    "menuProvider",
+    function($stateProvider, menuProvider) {
+        // enregistrement de l'etat logs
+        $stateProvider.state("rest-client.error-types", {
+            entryName: "Erreurs fonctionelles",
+            url: "/error-types",
+            templateUrl: "./modules/errorTypes/errorTypes.html",
+            controller: "errorTypesController",
+            controllerAs: "ctrl",
+            resolve: {
+                errorTypes: [
+                    "errorTypeService",
+                    function(errorTypeService) {
+                        return errorTypeService.get();
+                    }
+                ]
+            }
+        });
+
+        // enregistrement de l'entrée mennue Logs
+        menuProvider.add({
+            stateName: "rest-client.error-types",
+            name: "Erreurs fonctionelles",
+            icon: "exclamation",
+            importance: 100
+        });
+    }
+]);
---
+++ src/modules/errorTypes/errorTypes.js
@@ -0,0 +1,72 @@
+angular.module("rest-client").controller("errorTypesController", [
+    "$scope",
+    "errorTypes",
+    "errorTypeService",
+    "swal",
+    "toaster",
+    function($scope, errorTypes, errorTypeService, swal, toaster) {
+        $scope.errorType = {}; // élément contenant un nouveau type d'erreur
+        $scope.errorTypes = errorTypes; // liste des types d'erreurs
+
+        // ajoute ou modifie une errorType
+        this.addType = function(data) {
+            var promise,
+                phantom = angular.isUndefined(data._id);
+
+            // on drive la méthode en fonction du type d'objet transmis
+            if (phantom) {
+                promise = errorTypeService.post(data);
+            } else {
+                promise = errorTypeService.put(data);
+            }
+
+            promise
+                .then(function(res) {
+                    if (phantom) {
+                        // dans le cas d'une créa on l'ajoute au tableau
+                        $scope.errorTypes.unshift(data);
+                        $scope.errorType = {};
+                    } else {
+                        // dans le cas d'une modification on le met à jour
+                        data = res;
+                    }
+                    // dans tous les cas on envoi une notification de réussite
+                    toaster.success(
+                        "Sauvegarde réussie",
+                        "Type enregistré avec succès"
+                    );
+                })
+                .catch(function(err) {
+                    toaster.error("Erreur", err);
+                    throw err;
+                });
+        };
+
+        // supprime un errorType
+        this.deleteType = function(type) {
+            swal
+                .confirmDelete({
+                    title: "Supression",
+                    text:
+                        "Êtes vous sûre de vouloir supprimer ce type d'erreur ?",
+                    closeOnConfirm: false,
+                    closeOnCancel: false
+                })
+                .then(() => {
+                    return errorTypeService.delete(type);
+                })
+                .then(() => {
+                    $scope.errorTypes.splice(
+                        $scope.errorTypes.indexOf(type),
+                        1
+                    );
+                })
+                .catch(err => {
+                    if (err !== true) {
+                        toaster.error("Erreur", err);
+                        throw err;
+                    }
+                });
+        };
+    }
+]);
