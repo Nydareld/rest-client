7468d6aa99fab33eb80d6a22a0ca48af31467b78
Théo Guerin <theo.guerin.pro@gmail.com>
2018-07-09 14:14:37 +0200

    passage en /error

--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,6 @@
 angular.module("rest-client").constant("globals", {
     plApi: {
-        baseUrl: "/api/v1/error-manager"
+        baseUrl: "/api/v1/error"
     },
     debug: true,
     debugRouter: false,
